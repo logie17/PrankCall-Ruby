1536c2ccd2c920986a71d0bfa05b989a24ba754c
Logan Bell <loganbell@gmail.com>
2013-09-20 23:45:38 -0700

    Doing a crappy job monkey patching

--- lib/prankcall.rb
+++ lib/prankcall.rb
@@ -1,9 +1,15 @@
 class PrankCall
   VERSION = '0.0.1'
+  require 'socket'
 
-  def initialize(param)
-    @host = param[:host]
-    @port = param[:port]
+  def initialize(params)
+    @host = params[:host]
+    @port = params[:port]
+  end
+
+  def get(params)
+    TCPSocket.open(@host,@port)
+    1
   end
 
 end
--- spec/prank_spec.rb
+++ spec/prank_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 describe PrankCall do
   it "Should do a basic GET request" do
-    prank = PrankCall.new(:host => 'http://127.0.0.1', :port => 3450 )
+    open_params = []
+    require 'socket'
+    class TCPSocket;end
+    TCPSocket.class_eval { define_method(:open) { |*params| open_params.push[*params] } }
+
+    prank = PrankCall.new(:host => '127.0.0.1', :port => 3450 )
     expect(prank).to be_an_instance_of(PrankCall)
+
+    expect(prank.get(:path => '/', :params => { 'foo' => 'bar' })).to eq 1
+    expect(open_params[0]).to eq '127.0.0.1'
+    expect(open_params[1]).to eq 3450
+
+
   end
 end
