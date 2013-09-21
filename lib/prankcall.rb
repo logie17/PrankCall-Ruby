3ac920396ceff1e7344a6eb7fb06b87eddf6bf0c
Logan Bell <loganbell@gmail.com>
2013-09-20 23:07:23 -0700

    Stopped version warnings

--- lib/prankcall.rb
+++ lib/prankcall.rb
@@ -1,8 +1,10 @@
 class PrankCall
   VERSION = '0.0.1'
+
   def initialize(param)
     @host = param[:host]
     @port = param[:port]
   end
+
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,1 @@
-require 'PrankCall'
+require 'prankcall'
